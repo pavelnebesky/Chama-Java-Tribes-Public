4ceedec398ee53e0cc9206658dd8361273b4ce00
Pavel Nebesky <pavel@nebesky.eu>
2019-12-09 15:02:08 +0100

    feat: update HomeController
    update HomeController, now there is no need for index.html, Hello World is printed directly from controller

--- TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/controllers/HomeController.java
+++ TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/controllers/HomeController.java
@@ -1,12 +1,17 @@
 package com.greenfoxacademy.TribesBackend.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class HomeController {
 
-    @GetMapping(value="/")
-    public String index() {
-        return "index";
+    @RequestMapping("/")
+    public @ResponseBody
+    String greeting() {
+        return "Hello World";
     }
 
 }
