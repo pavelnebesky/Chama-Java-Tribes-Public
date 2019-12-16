1d2f17e835808570a9105149c9b7a5a02feb74c9
Marek <marekpoliak0@gmail.com>
2019-12-16 17:07:47 +0100

    :sparkle: feat: add filter

--- TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/services/AuthenticationService.java
+++ TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/services/AuthenticationService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
@@ -14,10 +15,12 @@ import static com.greenfoxacademy.TribesBackend.constants.SecurityConstants.*;
 @Service
 public class AuthenticationService {
 
+    public static final List<String> publicEndpoints = List.of("/login", "register");
+
     @Autowired
     private UserRepository userRepository;
 
-    public String generateJWT(String ip, Long id){
+    public String generateJWT(String ip, Long id) {
         Map<String, Object> headerMap = Map.of(IP_CLAIM, ip);
         return JWT.create()
                 .withHeader(headerMap)
