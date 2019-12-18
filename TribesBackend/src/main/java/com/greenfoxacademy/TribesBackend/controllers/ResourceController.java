0ce2e360d0f5a19a45499e3fe9140d230d999a2f
Jakub Jenc <jajenc@seznam.cz>
2019-12-18 17:29:31 +0100

    Add improved controller

--- TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/controllers/ResourceController.java
+++ TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/controllers/ResourceController.java
@@ -1,12 +1,20 @@
 package com.greenfoxacademy.TribesBackend.controllers;
 
+import com.greenfoxacademy.TribesBackend.models.Kingdom;
+import com.greenfoxacademy.TribesBackend.services.ResourceService;
+import com.greenfoxacademy.TribesBackend.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ResourceController {
+    @Autowired
+    private ResourceService resourceService;
+
     @GetMapping("/kingdom/resources")
-    public String getResource() {
+    public String getResource(Kingdom kingdom) {
+        resourceService.getResources(kingdom);
         return "something";
     }
 
--- TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/services/ResourceService.java
+++ TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/services/ResourceService.java
@@ -1,12 +1,15 @@
 package com.greenfoxacademy.TribesBackend.services;
 
+import com.greenfoxacademy.TribesBackend.models.Kingdom;
 import com.greenfoxacademy.TribesBackend.models.Resource;
 import com.greenfoxacademy.TribesBackend.repositories.KingdomRepository;
 import com.greenfoxacademy.TribesBackend.repositories.ResourceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ResourceService {
     @Autowired
     private ResourceRepository resourceRepository;
@@ -16,7 +19,7 @@ public class ResourceService {
         return resourceRepository.findByName(name) != null;
     }
 
-    public List<Resource> getResources(){
-        return getResources();
+    public List<Resource> getResources(Kingdom kingdom){
+        return kingdomRepository.getKingdomsResources(kingdom);
     }
 }
