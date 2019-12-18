95d4fb481fd9051031b7f2758b5d40ad1a08d35e
Pavel Nebesky <pavel@nebesky.eu>
2019-12-18 16:15:43 +0100

    feat: :sparkles: add Building model and update Kingdom model

---
+++ TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/models/Building.java
@@ -0,0 +1,25 @@
+package com.greenfoxacademy.TribesBackend.models;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@Entity
+public class Building {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private enum Type {
+        townhall,
+        farm,
+        mine,
+        barracks
+    }
+    private Timestamp started_at;
+    private Timestamp finished_at;
+    @ManyToOne
+    private Kingdom kingdom;
+}
