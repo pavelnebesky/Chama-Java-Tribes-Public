43d1d763251b3aaab799703583fa9e2415434552
Marek <marekpoliak0@gmail.com>
2019-12-19 13:57:26 +0100

    :art: add dividers for variables

--- TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/exceptions/IdNotFoundException.java
+++ TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/exceptions/IdNotFoundException.java
@@ -3,6 +3,6 @@ package com.greenfoxacademy.TribesBackend.exceptions;
 public class IdNotFoundException extends FrontendException {
 
     public IdNotFoundException(Long id) {
-        super("Id " + id + " not found!", 404);
+        super("Id \'" + id + "\' not found!", 404);
     }
 }
--- TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/exceptions/NoSuchEmailException.java
+++ TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/exceptions/NoSuchEmailException.java
@@ -3,6 +3,6 @@ package com.greenfoxacademy.TribesBackend.exceptions;
 public class NoSuchEmailException extends FrontendException {
 
     public NoSuchEmailException(String email) {
-        super("No such email: " + email + "!", 401);
+        super("No such email: \'" + email + "\'!", 401);
     }
 }
