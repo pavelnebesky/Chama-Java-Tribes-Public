b53e5dc22d1fa17a178a8024b3834ba0527114b6
Marek <marekpoliak0@gmail.com>
2019-12-24 13:10:44 +0100

    :hammer: refactor error response format

--- TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/services/ExceptionService.java
+++ TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/services/ExceptionService.java
@@ -3,11 +3,15 @@ package com.greenfoxacademy.TribesBackend.services;
 import com.greenfoxacademy.TribesBackend.exceptions.FrontendException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 
 @Service
 public class ExceptionService {
 
     public ResponseEntity handleResponseWithException(FrontendException e) {
-        return ResponseEntity.status(e.getSc()).body(e.getMessage());
+        ModelMap modelMap=new ModelMap();
+        modelMap.addAttribute("status", "error");
+        modelMap.addAttribute("error", e.getMessage());
+        return ResponseEntity.status(e.getSc()).body(modelMap);
     }
 }
