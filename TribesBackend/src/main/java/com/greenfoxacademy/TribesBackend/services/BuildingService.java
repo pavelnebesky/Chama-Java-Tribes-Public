9678fd130cf8fac616941eb0c8d7999789337701
Pavel Nebesky <pavel@nebesky.eu>
2020-01-05 22:56:59 +0100

    feat: :sparkles: leaderboard getting closer

--- TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/services/BuildingService.java
+++ TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/services/BuildingService.java
@@ -8,11 +8,12 @@ import com.greenfoxacademy.TribesBackend.repositories.KingdomRepository;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @Getter
@@ -50,11 +51,30 @@ public class BuildingService {
     public Map<String, Integer> getLeaderboard()
     {
         Map<String, Integer> map = new HashMap<String, Integer>();
+
         for (Kingdom kingdom:kingdomRepository.findAll()
              ) {
             map.put(kingdom.getName(), kingdom.getBuildings().size());
         }
-        return map;
+
+        Map<String, Integer> sortedByCount = map.entrySet()
+                .stream()
+                .sorted((Map.Entry.<String, Integer>comparingByValue().reversed()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+        var keys = sortedByCount.keySet().toArray();
+        var values = sortedByCount.values().toArray();
+
+        //String temp = keys[0].toString();
+        //Integer tempi = values[0].toString();
+
+//        List<ModelMap> leaderboards = null;
+//        for (int i=0; i<sortedByCount.size(); i++) {
+//        leaderboards.add(new ModelMap().addAttribute("kingdomname", sortedByCount.keySet().iterator().next()));
+//        leaderboards.add(new ModelMap().addAttribute("buildings", sortedByCount.values().toArray()[i]));
+//        }
+
+        return sortedByCount;
     }
 
     public Building createAndReturnBuilding(long userId, String type) {
