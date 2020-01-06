6563e2c2790832c730f2887fabd73789640a7955
Marek <marekpoliak0@gmail.com>
2020-01-06 16:22:18 +0100

    :art: delete not needed imports

--- TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/filters/JWTAuthenticationFilter.java
+++ TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/filters/JWTAuthenticationFilter.java
@@ -2,13 +2,9 @@ package com.greenfoxacademy.TribesBackend.filters;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.greenfoxacademy.TribesBackend.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
