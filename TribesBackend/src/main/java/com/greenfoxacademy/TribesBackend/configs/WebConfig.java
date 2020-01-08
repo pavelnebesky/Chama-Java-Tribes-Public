414a576f1f078a586565c24900750718931ba805
Marek <marekpoliak0@gmail.com>
2020-01-08 12:49:50 +0100

    :art: delete unnecesary import

--- TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/configs/WebConfig.java
+++ TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/configs/WebConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.util.List;
-
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
