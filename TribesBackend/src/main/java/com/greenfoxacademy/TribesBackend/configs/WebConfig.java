61da4b3af6db358eb2b320438a78b314d4630760
Marek <marekpoliak0@gmail.com>
2020-01-08 12:48:07 +0100

    :sparkles: feat: secure the server with CORS

---
+++ TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/configs/WebConfig.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy.TribesBackend.configs;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+}
