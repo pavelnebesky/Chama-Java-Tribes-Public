3d2cf30d3423bc9c1fe73bafcdf2dae3071aaf01
Marek <marekpoliak0@gmail.com>
2020-01-09 14:56:41 +0100

    :hammer: make fb redirect public for heroku

--- TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/constants/FacebookConstants.java
+++ TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/constants/FacebookConstants.java
@@ -3,5 +3,5 @@ package com.greenfoxacademy.TribesBackend.constants;
 public class FacebookConstants {
     public static final String APP_ID = "558093108347119";
     public static final String APP_SECRET = "45f3212797329b800873fdb783ae1193";
-    public static final String REDIRECT_URI_FOR_FACEBOOK = "http://localhost:8080/facebook/authentication";
+    public static final String REDIRECT_URI_FOR_FACEBOOK = "https://tribes-java-backend.herokuapp.com/facebook/authentication";
 }
