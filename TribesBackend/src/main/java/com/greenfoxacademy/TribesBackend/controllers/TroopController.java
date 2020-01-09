73e17802c15bce0242e1896900545d7105de57be
Jaroslav Micke <Jamicke@seznam.cz>
2020-01-09 10:45:10 +0100

    beforeMergeExeptions

--- TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/controllers/TroopController.java
+++ TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/controllers/TroopController.java
@@ -1,20 +1,40 @@
 package com.greenfoxacademy.TribesBackend.controllers;
 
+import com.greenfoxacademy.TribesBackend.exceptions.NotEnoughGoldException;
+import com.greenfoxacademy.TribesBackend.models.Troop;
 import com.greenfoxacademy.TribesBackend.services.TroopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class TroopController {
-
     @Autowired
     TroopService troopService;
+
     @GetMapping("kingdom/troops")
-    public ResponseEntity getTroopsin(HttpServletRequest request){
+    public ResponseEntity getAllTroops(HttpServletRequest request){
         return ResponseEntity.ok(troopService.getModelMapOfAllTroopsByUserId(request));
+    }
+
+    @GetMapping("/kingdom/troops/[troopId]")
+    public ResponseEntity getTrooperById(HttpServletRequest request, long troopId){
+        return ResponseEntity.ok(troopService.getTrooperById(troopId));
+    }
+
+    @PostMapping("/kingdom/troops")
+    public ResponseEntity createNewTrooper(HttpServletRequest request){
+        Long userId = troopService.getUtilityService().getIdFromToken(request);
+        try {
+            newTroop = troopService.createAndReturnNewTroop(userId);
+        }catch (NotEnoughGoldException e){
+            return troopService.getUtilityService().
+        }
+
     }
 }
--- TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/services/TroopService.java
+++ TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/services/TroopService.java
@@ -1,5 +1,7 @@
 package com.greenfoxacademy.TribesBackend.services;
 
+import com.greenfoxacademy.TribesBackend.exceptions.NotEnoughGoldException;
+import com.greenfoxacademy.TribesBackend.models.Kingdom;
 import com.greenfoxacademy.TribesBackend.models.Troop;
 import com.greenfoxacademy.TribesBackend.repositories.TroopRepository;
 import lombok.Getter;
@@ -9,6 +11,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.ui.ModelMap;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+import static com.greenfoxacademy.TribesBackend.enums.BuildingType.barracks;
+import static com.greenfoxacademy.TribesBackend.enums.ResourceType.gold;
 
 @Getter
 @Setter
@@ -18,8 +25,14 @@ public class TroopService {
     private TroopRepository troopRepository;
     @Autowired
     private UtilityService utilityService;
+    @Autowired
+    private BuildingService buildingService;
+    @Autowired
+    private KingdomService kingdomService;
 
-    public Troop getTroopById(long trooperId){
+    static final long TRAINING_TIME = 60000;
+
+    public Troop getTrooperById(long trooperId){
         return troopRepository.findTrooperById(trooperId);
     }
 
@@ -35,6 +48,32 @@ public class TroopService {
 
     public Iterable<Troop> getAllTroopsByToken(HttpServletRequest request){
         return getAllTroopsByUserId(getUtilityService().getIdFromToken(request));
+    }
+
+    public Troop saveTroop(Troop troop){
+        troopRepository.save(troop);
+        return troop;
+    }
+
+    public Troop createAndReturnNewTroop(long userId) throws NotEnoughGoldException{
+        //int barraksLevel = buildingService.getAllBuildingsByUserId(userId).toList().stream().filter(b -> b.getType().equals(barracks)).findAny().get().getLevel();
+        int kingdomsGold = buildingService.getKingdomRepository().findByUserId(userId).getResources().stream().filter(r -> r.getType().equals(gold)).findAny().get().getAmount();
+        int barraksLevel = StreamSupport.stream(buildingService.getAllBuildingsByUserId(userId).spliterator(), false).filter(b -> b.getType().equals(barracks)).findAny().get().getLevel();
+        if (kingdomsGold >= 10) {
+            Troop newTroop = new Troop();
+            newTroop.setHp(barraksLevel * 10);
+            newTroop.setAttack(1);
+            newTroop.setDefence(1);
+            newTroop.setStarted_at(System.currentTimeMillis());
+            newTroop.setFinished_at(newTroop.getStarted_at() + TRAINING_TIME);
+            newTroop.setKingdom(kingdomService.getKingdomByUserId(userId));
+            saveTroop(newTroop);
+            Kingdom kingdomToUpdate = buildingService.getKingdomRepository().findByUserId(userId);
+            List<Troop> kingdomTroops = troopRepository.findAllTroopsByKingdomUserId(userId);
+            kingdomTroops.add(newTroop);
+            return newTroop;
+        }else throw new NotEnoughGoldException();
+
     }
 }
 
