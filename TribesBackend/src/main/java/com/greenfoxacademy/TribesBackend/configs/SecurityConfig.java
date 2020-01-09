6c29f81b100aca5de7013e6c89426ae67a4624be
Pavel Nebesky <pavel@nebesky.eu>
2020-01-09 13:12:12 +0100

    feat: :lipstick: update list of origins

--- TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/configs/SecurityConfig.java
+++ TribesBackend/src/main/java/com/greenfoxacademy/TribesBackend/configs/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("retros-tribes.netlify.com", "tribes-java-backend.herokuapp.com", "localhost:8080"));
+        configuration.setAllowedOrigins(List.of("retros-tribes.netlify.com", "tribes-java-backend.herokuapp.com", "http://localhost:8080"));
         configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
