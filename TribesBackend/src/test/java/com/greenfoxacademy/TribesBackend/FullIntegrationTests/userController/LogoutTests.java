1539746c01a1c67258608d5a72697cbaa4477043
Marek <marekpoliak0@gmail.com>
2020-01-18 14:18:01 +0100

    feat: :sparkles: add logout test

---
+++ TribesBackend/src/test/java/com/greenfoxacademy/TribesBackend/FullIntegrationTests/userController/LogoutTests.java
@@ -0,0 +1,72 @@
+package com.greenfoxacademy.TribesBackend.FullIntegrationTests.userController;
+
+import com.greenfoxacademy.TribesBackend.exceptions.EmailAlreadyTakenException;
+import com.greenfoxacademy.TribesBackend.exceptions.FrontendException;
+import com.greenfoxacademy.TribesBackend.models.User;
+import com.greenfoxacademy.TribesBackend.utilityMethods.UtilityMethods;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(
+        locations = "classpath:application-testing.properties")
+public class LogoutTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private UtilityMethods utilityMethods;
+    private String token;
+    private String ip = "";
+
+    @BeforeEach
+    public void before() {
+        utilityMethods.clearDB();
+    }
+
+    @AfterEach
+    public void after() {
+        utilityMethods.clearDB();
+    }
+
+    @Test
+    public void successfullLogoutTest() throws Exception {
+        User user=utilityMethods.createEverything("some@email.com", "blah", 0,0, List.of());
+        token = utilityMethods.generateToken(user.getUsername(), ip, user.getId());
+        mockMvc.perform(post("/logout")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + token)
+                .with(request -> {
+                    request.setRemoteAddr(ip);
+                    return request;
+                })
+                .content("{ }"))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/kingdom")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + token)
+                .with(request -> {
+                    request.setRemoteAddr(ip);
+                    return request;
+                })
+                .content("{ }"))
+                .andExpect(status().is(401))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+}
