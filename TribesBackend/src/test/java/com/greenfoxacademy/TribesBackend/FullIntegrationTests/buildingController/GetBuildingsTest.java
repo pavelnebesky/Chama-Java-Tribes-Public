5d18fc6b80474e4239c9df8f49f784c119729d57
Pavel Nebesky <pavel@nebesky.eu>
2020-01-20 12:22:26 +0100

    feat: :sparkles: add first get buildings test

---
+++ TribesBackend/src/test/java/com/greenfoxacademy/TribesBackend/FullIntegrationTests/buildingController/GetBuildingsTest.java
@@ -0,0 +1,74 @@
+package com.greenfoxacademy.TribesBackend.FullIntegrationTests.buildingController;
+
+import com.greenfoxacademy.TribesBackend.enums.BuildingType;
+import com.greenfoxacademy.TribesBackend.exceptions.FrontendException;
+import com.greenfoxacademy.TribesBackend.exceptions.MissingParamsException;
+import com.greenfoxacademy.TribesBackend.models.Building;
+import com.greenfoxacademy.TribesBackend.models.User;
+import com.greenfoxacademy.TribesBackend.utilityMethods.UtilityMethods;
+import com.greenfoxacademy.TribesBackend.utilityMethods.UtilityMethods;
+import org.hamcrest.Matchers;
+import org.json.JSONArray;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.greenfoxacademy.TribesBackend.enums.BuildingType.mine;
+import static com.greenfoxacademy.TribesBackend.enums.BuildingType.townhall;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.Is.isA;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import java.util.List;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(
+        locations = "classpath:application-testing.properties")
+
+public class GetBuildingsTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private UtilityMethods utilityMethods;
+    private String token;
+    private String ip = "";
+    private User user;
+
+    @BeforeEach
+    public void before() {
+        user = utilityMethods.createEverything("john@doe.com", "Johns kingdom", 1000, 1000, List.of(townhall, mine));
+        token = utilityMethods.generateToken("something@sth.com", ip, user.getId());
+    }
+
+    @AfterEach
+    public void after() {
+        utilityMethods.clearDB();
+    }
+
+    @Test
+    public void getBuildings() throws Exception {
+        mockMvc.perform(get("/kingdom/buildings")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + token)
+                .with(request -> {
+                    request.setRemoteAddr(ip);
+                    return request;
+                })
+                .content("{}"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.buildings", Matchers.any(List.class)))
+                .andExpect(jsonPath("$.buildings[0].type", is("townhall")))
+                .andExpect(jsonPath("$.buildings[1].type", is("mine")));
+    }
+}
