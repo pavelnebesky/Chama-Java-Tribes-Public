247882add259829dc723abb8c03e908842ae330d
Marek <marekpoliak0@gmail.com>
2020-01-22 13:13:36 +0100

    :hammer: :art: :hammer: :art: refactor tests

--- TribesBackend/src/test/java/com/greenfoxacademy/TribesBackend/testUtilities/UtilityMethods.java
+++ TribesBackend/src/test/java/com/greenfoxacademy/TribesBackend/testUtilities/UtilityMethods.java
@@ -1,24 +1,29 @@
-package com.greenfoxacademy.TribesBackend.utilityMethods;
+package com.greenfoxacademy.TribesBackend.testUtilities;
 
 import com.auth0.jwt.JWT;
 import com.greenfoxacademy.TribesBackend.enums.BuildingType;
 import com.greenfoxacademy.TribesBackend.enums.ResourceType;
+import com.greenfoxacademy.TribesBackend.exceptions.FrontendException;
 import com.greenfoxacademy.TribesBackend.models.*;
 import com.greenfoxacademy.TribesBackend.repositories.*;
 import com.greenfoxacademy.TribesBackend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 import static com.greenfoxacademy.TribesBackend.constants.BuildingConstants.BUILDING_TIMES;
 import static com.greenfoxacademy.TribesBackend.constants.ResourceConstants.GOLD_PER_MINUTE;
 import static com.greenfoxacademy.TribesBackend.constants.SecurityConstants.*;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @Configuration
 public class UtilityMethods {
@@ -52,8 +57,8 @@ public class UtilityMethods {
         kingdom.setLocation(new Location());
         kingdom.setName(kingdomName);
         List<Building> buildings = new ArrayList<>();
-        for (int i = 0; i < types.size(); i++) {
-            buildings.add(setupBuilding(types.get(i), kingdom));
+        for (BuildingType type : types) {
+            buildings.add(setupBuilding(type, kingdom));
         }
         kingdom.setBuildings(buildings);
         kingdomRepository.save(kingdom);
@@ -119,5 +124,40 @@ public class UtilityMethods {
         troopRepository.deleteAll();
         blacklistedToken.deleteAll();
         authGrantAccessToken.deleteAll();
+    }
+
+    public MockHttpServletRequestBuilder buildNonAuthRequest(String endpoint, String httpMethod, String content) throws Exception {
+        return httpMethodDecider(endpoint, httpMethod)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+    }
+
+    public MockHttpServletRequestBuilder buildAuthRequest(String endpoint, String httpMethod, String token, String ip, String content) throws Exception {
+        return httpMethodDecider(endpoint, httpMethod)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + token)
+                .with(request -> {
+                    request.setRemoteAddr(ip);
+                    return request;
+                })
+                .content(content);
+    }
+
+    public MockHttpServletRequestBuilder httpMethodDecider(String endpoint, String httpMethod) {
+        Map<String, MockHttpServletRequestBuilder> httpMethods = new HashMap<>() {
+            {
+                put("get", MockMvcRequestBuilders.get(endpoint));
+                put("put", MockMvcRequestBuilders.put(endpoint));
+                put("post", MockMvcRequestBuilders.post(endpoint));
+            }
+        };
+        return httpMethods.get(httpMethod);
+    }
+
+    public ResultActions exceptionExpectations(ResultActions resultActions, FrontendException e) throws Exception {
+        return resultActions.andExpect(status().is(e.getSc()))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status", is("error")))
+                .andExpect(jsonPath("$.error", is(e.getMessage())));
     }
 }
