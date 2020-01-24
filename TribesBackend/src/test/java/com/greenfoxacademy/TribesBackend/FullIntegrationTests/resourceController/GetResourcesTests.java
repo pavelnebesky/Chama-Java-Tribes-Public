73e89e42d54e16955fb6ba7e21eed1cfa1cb31ce
Jakub Jenc <jajenc@seznam.cz>
2020-01-24 10:06:58 +0100

    feat: :heavy_check_mark: Make resources test pass

--- TribesBackend/src/test/java/com/greenfoxacademy/TribesBackend/FullIntegrationTests/resourceController/GetResourcesTests.java
+++ TribesBackend/src/test/java/com/greenfoxacademy/TribesBackend/FullIntegrationTests/resourceController/GetResourcesTests.java
@@ -1,17 +1,23 @@
 package com.greenfoxacademy.TribesBackend.FullIntegrationTests.resourceController;
 
+import com.greenfoxacademy.TribesBackend.enums.BuildingType;
 import com.greenfoxacademy.TribesBackend.exceptions.FrontendException;
 import com.greenfoxacademy.TribesBackend.exceptions.IdNotFoundException;
 import com.greenfoxacademy.TribesBackend.models.Building;
+import com.greenfoxacademy.TribesBackend.models.Kingdom;
 import com.greenfoxacademy.TribesBackend.models.User;
 import com.greenfoxacademy.TribesBackend.repositories.BuildingRepository;
-import com.greenfoxacademy.TribesBackend.utilityMethods.UtilityMethods;
+import com.greenfoxacademy.TribesBackend.testUtilities.UtilityMethods;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Matches;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -23,6 +29,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-testing.properties")
 public class GetResourcesTests {
 
     @Autowired
@@ -34,11 +43,12 @@ public class GetResourcesTests {
     private String token;
     private String ip = "";
     private User user;
+    private Kingdom kingdom;
 
     @BeforeEach
     public void before() {
-        user = utilityMethods.createEverything("frantisek@dobrota.com", "frantisek's kingdom", 1000, 1000, List.of(townhall, mine));
-        token = utilityMethods.generateToken("frantisek@dobrota.com", ip, user.getId());
+        user = utilityMethods.createEverything("something@sth.com", "kingdomName", 10000, 10000, List.of(BuildingType.townhall, BuildingType.barracks, BuildingType.mine, BuildingType.farm));
+        token = utilityMethods.generateToken("something@sth.com", ip, user.getId());
     }
 
     @AfterEach
@@ -47,72 +57,23 @@ public class GetResourcesTests {
     }
 
     @Test
-    public void getBuildings() throws Exception {
-        mockMvc.perform(get("/kingdom/resources")
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Bearer " + token)
-                .with(request -> {
-                    request.setRemoteAddr(ip);
-                    return request;
-                })
-                .content("{}"))
+    public void getResources() throws Exception {
+        mockMvc.perform(utilityMethods.buildAuthRequest("/kingdom/resources", "get", token, ip,
+                "{}"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.buildings", Matchers.any(List.class)))
-                .andExpect(jsonPath("$.buildings[0].type", is("townhall")))
-                .andExpect(jsonPath("$.buildings[1].type", is("mine")));
+                .andExpect(jsonPath("$.resources", Matchers.any(List.class)))
+                .andExpect(jsonPath("$.resources[0].type", is("gold")))
+                .andExpect(jsonPath("$.resources[0].amount", is(10000)))
+                .andExpect(jsonPath("$.resources[1].type", is("food")))
+                .andExpect(jsonPath("$.resources[1].amount", is(10000)));
     }
 
     @Test
-    public void getSpecifiedBuilding() throws Exception {
-        Long townhallId = ((List<Building>) buildingRepository.findAll())
-                .stream().filter(b -> b.getType().equals(townhall)).findAny().get().getId();
-        mockMvc.perform(get("/kingdom/buildings/" + townhallId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Bearer " + token)
-                .with(request -> {
-                    request.setRemoteAddr(ip);
-                    return request;
-                })
-                .content("{}"))
+    public void getResourcesType() throws Exception {
+        mockMvc.perform(utilityMethods.buildAuthRequest("/kingdom/resources/{resourceType}", "get", token, ip, "{}"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.type", is("townhall")))
-                .andExpect(jsonPath("$.level", is(1)));
-    }
-
-    @Test
-    public void getNotExistingBuilding() throws Exception {
-        Long id = 99339933L;
-        FrontendException e = new IdNotFoundException(id);
-        mockMvc.perform(get("/kingdom/buildings/" + id)
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Bearer " + token)
-                .with(request -> {
-                    request.setRemoteAddr(ip);
-                    return request;
-                })
-                .content("{}"))
-                .andExpect(status().is(e.getSc()))
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.status", is("error")))
-                .andExpect(jsonPath("$.error", is(e.getMessage())));
-    }
-
-    @Test
-    public void getLeaderboard() throws Exception {
-        mockMvc.perform(get("/leaderboard/buildings")
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Bearer " + token)
-                .with(request -> {
-                    request.setRemoteAddr(ip);
-                    return request;
-                })
-                .content("{}"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.leaderboard", Matchers.any(List.class)))
-                .andExpect(jsonPath("$.leaderboard[0].kingdomname", is("Johns kingdom")))
-                .andExpect(jsonPath("$.leaderboard[0].buildings", is(2)));
+                .andExpect(jsonPath("$.resourceType", is(getResourcesType())));
     }
 }
